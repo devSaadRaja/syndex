263663c586b213be0d32624956e67455c4ad3d32
saadraja-aleh <saadraja@aleh.tech>
2024-06-05 19:56:46 +0500

    added testing scripts for multi collateral swap

--- test/SMX.t.sol
+++ test/SMX.t.sol
@@ -45,12 +45,26 @@ contract SMXTest is Setup {
     function setUp() public override {
         super.setUp();
 
-        vm.startPrank(address(issuer));
-        synthsUSD.issue(owner, 1000 ether);
-        synthsUSD.issue(user1, 1000 ether);
-        vm.stopPrank();
+        // vm.startPrank(address(issuer));
+        // synthsUSD.issue(owner, 1000 ether);
+        // synthsUSD.issue(user1, 1000 ether);
+        // vm.stopPrank();
 
         vm.startPrank(owner);
+        smx.transfer(user6, 1000 ether);
+        smx.transfer(user7, 1000 ether);
+        smx.transfer(user8, 1000 ether);
+        vm.stopPrank();
+
+        vm.startPrank(user7);
+        synthetix.createMaxSynths();
+        vm.stopPrank();
+
+        vm.startPrank(user8);
+        synthetix.createMaxSynths();
+        vm.stopPrank();
+
+        vm.startPrank(user7);
 
         synthetix.executeExchange("sUSD", 500 ether, "sETH");
 
@@ -65,15 +79,15 @@ contract SMXTest is Setup {
             50 ether,
             0,
             0,
-            owner,
+            user7,
             block.timestamp + 10 minutes
         );
 
-        supplySchedule2 = new SupplySchedule(owner, treasury);
+        supplySchedule2 = new SupplySchedule(user7, treasury);
         // vSmxRedeemer = new vSMXRedeemer(address(smx), address(smx));
-        rewardEscrow2 = new RewardEscrow(owner, address(smx));
+        rewardEscrow2 = new RewardEscrow(user7, address(smx));
         multipleMerkleDistributor = new MultipleMerkleDistributor(
-            owner,
+            user7,
             address(smx),
             address(rewardEscrow2)
         );
@@ -87,7 +101,7 @@ contract SMXTest is Setup {
             50 ether,
             0,
             0,
-            owner,
+            user7,
             block.timestamp + 10 minutes
         );
 
@@ -106,16 +120,18 @@ contract SMXTest is Setup {
         //     address(clipperExchangeInterface)
         // );
 
-        token = new Token("Token", "TKN", owner, 1_000_000 ether);
+        token = new Token("Token", "TKN", user7, 1_000_000 ether);
         synthSwap2 = new SynthSwap(
             address(proxysUSD),
             address(swapRouter), // routerV4, aggregationRouterV4
             address(addressResolver),
-            owner,
+            user7,
             treasury
         );
 
         token.transfer(user5, 1000 ether);
+        token.transfer(user6, 1000 ether);
+        token.transfer(user8, 1000 ether);
 
         // ! MAKE POOL ---
 
@@ -140,32 +156,32 @@ contract SMXTest is Setup {
             50 ether
         );
 
-        int24 tickSpacing = v3Pool.tickSpacing();
-        uint128 liquidity = v3Pool.liquidity();
+        // int24 tickSpacing = v3Pool.tickSpacing();
+        // uint128 liquidity = v3Pool.liquidity();
         uint24 fee = v3Pool.fee();
-        (
-            uint160 sqrtPriceX96,
-            int24 tick,
-            uint16 observationIndex,
-            uint16 observationCardinality,
-            uint16 observationCardinalityNext,
-            uint8 feeProtocol,
-            bool unlocked
-        ) = v3Pool.slot0();
+        // (
+        //     uint160 sqrtPriceX96,
+        //     int24 tick,
+        //     uint16 observationIndex,
+        //     uint16 observationCardinality,
+        //     uint16 observationCardinalityNext,
+        //     uint8 feeProtocol,
+        //     bool unlocked
+        // ) = v3Pool.slot0();
 
-        console.log("liquidity BEFORE", liquidity);
-        console.log("tickSpacing", uint24(tickSpacing));
-        console.log("fee", fee);
-        console.log("sqrtPriceX96", sqrtPriceX96);
-        console.log("tick", uint24(tick));
-        console.log("observationIndex", uint16(observationIndex));
-        console.log("observationCardinality", uint16(observationCardinality));
-        console.log(
-            "observationCardinalityNext",
-            uint16(observationCardinalityNext)
-        );
-        console.log("feeProtocol", uint16(feeProtocol));
-        console.log("unlocked", unlocked);
+        // console.log("liquidity BEFORE", liquidity);
+        // console.log("tickSpacing", uint24(tickSpacing));
+        // console.log("fee", fee);
+        // console.log("sqrtPriceX96", sqrtPriceX96);
+        // console.log("tick", uint24(tick));
+        // console.log("observationIndex", uint16(observationIndex));
+        // console.log("observationCardinality", uint16(observationCardinality));
+        // console.log(
+        //     "observationCardinalityNext",
+        //     uint16(observationCardinalityNext)
+        // );
+        // console.log("feeProtocol", uint16(feeProtocol));
+        // console.log("unlocked", unlocked);
 
         // tickLower = nearestUsableTick(tick, tickSpacing) - tickSpacing * 2;
         // tickUpper = nearestUsableTick(tick, tickSpacing) + tickSpacing * 2;
@@ -188,7 +204,7 @@ contract SMXTest is Setup {
                 amount1Desired: 50 ether,
                 amount0Min: 0,
                 amount1Min: 0,
-                recipient: owner,
+                recipient: user7,
                 deadline: block.timestamp + 10 minutes
             });
 
@@ -198,7 +214,7 @@ contract SMXTest is Setup {
     }
 
     function testRouterV4() public {
-        vm.startPrank(owner);
+        vm.startPrank(user7);
 
         // ! SWAP ---
 
@@ -224,8 +240,8 @@ contract SMXTest is Setup {
         //     .SwapDescription({
         //         srcToken: address(proxysUSD),
         //         dstToken: address(token),
-        //         srcReceiver: payable(owner),
-        //         dstReceiver: payable(owner),
+        //         srcReceiver: payable(user7),
+        //         dstReceiver: payable(user7),
         //         amount: 1 ether,
         //         minReturnAmount: 0.01 ether,
         //         flags: 1,
@@ -240,18 +256,18 @@ contract SMXTest is Setup {
         //     _data
         // );
 
-        console.log("BEFORE token\n", token.balanceOf(owner));
-        console.log("BEFORE proxysETH\n", proxysETH.balanceOf(owner));
+        console.log("BEFORE token\n", token.balanceOf(user7));
+        console.log("BEFORE proxysETH\n", proxysETH.balanceOf(user7));
 
         IERC20(address(token)).approve(address(synthSwap2), 2 ether);
         synthSwap2.uniswapSwapInto("sETH", address(token), 1 ether, _data);
         // // IERC20(address(proxysUSD)).approve(address(synthSwap2), 10 ether);
         // // synthSwap2.uniswapSwapInto("sETH", address(proxysUSD), 10 ether, data);
 
-        console.log("AFTER uniswapSwapInto token\n", token.balanceOf(owner));
+        console.log("AFTER uniswapSwapInto token\n", token.balanceOf(user7));
         console.log(
             "AFTER uniswapSwapInto proxysETH\n",
-            proxysETH.balanceOf(owner)
+            proxysETH.balanceOf(user7)
         );
 
         // ? uniswapSwapOutOf
@@ -281,10 +297,10 @@ contract SMXTest is Setup {
             _dataOut
         );
 
-        console.log("AFTER uniswapSwapOutOf token\n", token.balanceOf(owner));
+        console.log("AFTER uniswapSwapOutOf token\n", token.balanceOf(user7));
         console.log(
             "AFTER uniswapSwapOutOf proxysETH\n",
-            proxysETH.balanceOf(owner)
+            proxysETH.balanceOf(user7)
         );
 
         ISwapRouter.ExactInputSingleParams memory paramsss = ISwapRouter
@@ -306,20 +322,20 @@ contract SMXTest is Setup {
         console.log();
         console.log();
 
-        console.log("BEFORE token\n", token.balanceOf(owner));
-        console.log("BEFORE proxysUSD\n", proxysUSD.balanceOf(owner));
+        console.log("BEFORE token\n", token.balanceOf(user7));
+        console.log("BEFORE proxysUSD\n", proxysUSD.balanceOf(user7));
 
         IERC20(address(token)).approve(address(synthSwap2), 2 ether);
         synthSwap2.uniswapSwapInto("sUSD", address(token), 1 ether, _dataaa);
 
-        console.log("AFTER token\n", token.balanceOf(owner));
-        console.log("AFTER proxysUSD\n", proxysUSD.balanceOf(owner));
+        console.log("AFTER token\n", token.balanceOf(user7));
+        console.log("AFTER proxysUSD\n", proxysUSD.balanceOf(user7));
 
         vm.stopPrank();
     }
 
     function testUniswapV3() public {
-        vm.startPrank(owner);
+        vm.startPrank(user7);
 
         // ! SWAP ---
 
@@ -329,7 +345,7 @@ contract SMXTest is Setup {
                 tokenIn: address(proxysUSD),
                 tokenOut: address(token),
                 fee: v3Pool.fee(),
-                recipient: owner,
+                recipient: user7,
                 deadline: block.timestamp + 10 minutes,
                 amountIn: 1 ether,
                 amountOutMinimum: 0,
@@ -345,7 +361,7 @@ contract SMXTest is Setup {
                 tokenIn: address(token),
                 tokenOut: address(proxysUSD),
                 fee: v3Pool.fee(),
-                recipient: owner,
+                recipient: user7,
                 deadline: block.timestamp + 10 minutes,
                 amountOut: 1 ether,
                 amountInMaximum: 10 ether,
@@ -358,16 +374,16 @@ contract SMXTest is Setup {
     }
 
     function testOnlyBurner() public {
-        vm.startPrank(user1);
+        vm.startPrank(user8);
         vm.expectRevert();
         smx.burn();
         vm.stopPrank();
 
-        vm.startPrank(owner);
-        smx.grantRole(keccak256("BURNER_ROLE"), user1);
+        vm.startPrank(user7);
+        smx.grantRole(keccak256("BURNER_ROLE"), user8);
         vm.stopPrank();
 
-        vm.startPrank(user1);
+        vm.startPrank(user8);
         assertEq(smx.balanceOf(reserveAddr), 200000 ether);
         smx.burn();
         assertEq(smx.balanceOf(reserveAddr), 100000 ether);
@@ -463,34 +479,145 @@ contract SMXTest is Setup {
             tradingRewards.getAvailableRewardsForAccountForPeriod(user5, 0)
         );
         console.log(
-            "getAvailableRewardsForAccountForPeriod(owner, 0)",
-            tradingRewards.getAvailableRewardsForAccountForPeriod(owner, 0)
+            "getAvailableRewardsForAccountForPeriod(user7, 0)",
+            tradingRewards.getAvailableRewardsForAccountForPeriod(user7, 0)
         );
 
         vm.stopPrank();
     }
 
+    function _consoleData(string memory str) internal view {
+        console.log();
+        console.log(str);
+        console.log(
+            IERC20(address(token)).balanceOf(user6),
+            "<-- token balanceOf(user6)"
+        );
+        console.log(
+            synthetix.transferableSynthetix(user6),
+            "<-- synthetix transferableSynthetix(user6)"
+        );
+        console.log(
+            IERC20(address(proxySNX)).balanceOf(user6),
+            "<-- SNX balanceOf(user6)"
+        );
+        console.log(
+            IERC20(address(proxysUSD)).balanceOf(user6),
+            "<-- sUSD balanceOf(user6)"
+        );
+        console.log(
+            IERC20(address(proxysETH)).balanceOf(user6),
+            "<-- sETH balanceOf(user6)"
+        );
+        console.log(address(user6).balance, "<-- ETH balance address(user6)");
+        console.log(
+            address(collateralETH).balance,
+            "<-- ETH balance address(collateralETH)"
+        );
+        console.log(
+            IERC20(address(smx)).balanceOf(address(user6)),
+            "<-- smx balanceOf(address(user6))"
+        );
+        console.log(
+            IERC20(address(smx)).balanceOf(address(collateralErc20)),
+            "<-- smx balanceOf(address(collateralErc20))"
+        );
+        console.log(
+            synthetixDebtShare.totalSupply(),
+            "<-- synthetixDebtShare.totalSupply()"
+        );
+        console.log(
+            synthetixDebtShare.balanceOf(user6),
+            "<-- synthetixDebtShare.balanceOf(user6)"
+        );
+        console.log(
+            synthetixDebtShare.calculateTotalSupplyForPeriod(1),
+            "<-- synthetixDebtShare.calculateTotalSupplyForPeriod(1)"
+        );
+        console.log(
+            collateralManager.state().totalLoans(),
+            "<-- collateralManager.state().totalLoans()"
+        );
+
+        (uint long, uint short) = collateralManager.state().totalIssuedSynths(
+            "sUSD"
+        );
+        console.log(long, "<-- totalIssuedSynths(sUSD) long");
+        console.log(short, "<-- totalIssuedSynths(sUSD) short");
+
+        (uint susdValue, ) = collateralManager.totalLongAndShort();
+        console.log(susdValue, "<-- susdValue");
+    }
+
+    function testMultiCollateralReturns() public {
+        vm.startPrank(user6);
+
+        _consoleData("--- BEFORE ---");
+
+        synthetix.createSynths(1 ether);
+
+        uint256 id = collateralETH.open{value: 1.5 ether}(1 ether, "sUSD");
+
+        smx.approve(address(collateralErc20), 50 ether);
+        uint256 idErc = collateralErc20.open(3 ether, 2 ether, "sUSD");
+
+        _consoleData("--- AFTER SWAP ---");
+
+        ISwapRouter.ExactInputSingleParams memory inputParams = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: address(token),
+                tokenOut: address(proxysUSD),
+                fee: v3Pool.fee(),
+                recipient: address(synthSwap),
+                deadline: block.timestamp + 10 minutes,
+                amountIn: 1 ether,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+        bytes memory _data = abi.encodeWithSelector(
+            ISwapRouter.exactInputSingle.selector,
+            inputParams
+        );
+        
+        IERC20(address(token)).approve(address(synthSwap), 1 ether);
+        synthSwap.uniswapSwapInto("sETH", address(token), 1 ether, _data);
+
+        _consoleData("--- AFTER SYNTH SWAP ---");
+
+        synthetix.burnSynths(1 ether);
+        // // synthetix.burnSynthsToTarget();
+
+        collateralETH.close(id);
+        collateralETH.claim(1.5 ether);
+
+        collateralErc20.close(idErc);
+
+        _consoleData("--- AFTER SYNTH BURN ---");
+
+        vm.stopPrank();
+    }
+
     function testSwap() public {
-        vm.startPrank(user1);
+        vm.startPrank(user8);
 
         console.log("BEFORE");
-        console.log("SMX", smx.balanceOf(user1));
-        console.log("WETH", IERC20(WETH).balanceOf(user1));
+        console.log("SMX", smx.balanceOf(user8));
+        console.log("WETH", IERC20(WETH).balanceOf(user8));
 
-        _swap(WETH, address(smx), 10 ether, user1);
+        _swap(WETH, address(smx), 10 ether, user8);
 
         console.log("AFTER");
-        console.log("SMX", smx.balanceOf(user1));
-        console.log("WETH", IERC20(WETH).balanceOf(user1));
+        console.log("SMX", smx.balanceOf(user8));
+        console.log("WETH", IERC20(WETH).balanceOf(user8));
 
         vm.stopPrank();
     }
 
     function testTax() public {
-        vm.startPrank(user1);
+        vm.startPrank(user8);
 
-        _swap(WETH, address(smx), 10 ether, user1); // BUY
-        _swap(address(smx), WETH, 5 ether, user1); // SELL
+        _swap(WETH, address(smx), 10 ether, user8); // BUY
+        _swap(address(smx), WETH, 5 ether, user8); // SELL
 
         console.log();
         console.log("threshold\n", smx.threshold());
@@ -508,10 +635,10 @@ contract SMXTest is Setup {
     }
 
     function testBlacklist() public {
-        vm.startPrank(owner);
-        smx.updateBlacklist(user1, true);
+        vm.startPrank(user7);
+        smx.updateBlacklist(user8, true);
         vm.stopPrank();
-        vm.startPrank(user1);
+        vm.startPrank(user8);
         vm.expectRevert("Address is blacklisted");
         smx.transfer(user3, 1 ether);
         vm.stopPrank();
